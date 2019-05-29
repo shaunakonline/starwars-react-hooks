57880c63df28c5d654aa624db6c87c36b85d8794
shaunaky <shaunaky@gmail.com>
2019-05-29 08:03:28 -0700

    v0.01 CharacterList added and api call to SWAPI list disploayed on first page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'; 
 import './App.css';
+import CharacterList from './components/CharacterList'
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    test: 0,
+    side: 'light',
+    starshipId: null,
+    characterId: null
+  };
+
+  handleSide = (side) => {
+    this.setState({side: side}); // can be written as this.setState({side})
+  }
+
+  handleStarshipSelector = (shipId) => {
+    this.setState({starshipId: shipId});
+  }
+  handleCharacterSelector = (e) => {
+    const characterId = e.target.value;
+    this.setState({characterId: characterId});
+  }
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">    
+       <p>Test</p>
+        <CharacterList
+          side = {this.state.side}
+          onCharacterSelector = {this.handleCharacterSelector}
+          selectedCharactor = {this.state.characterId}
+        />
+       <button onClick={this.handleSide.bind(this, 'light')}> Light Side </button>
+       <button onClick={this.handleSide.bind(this, 'dark')}> Dark Side </button>
+       <button onClick={this.handleStarshipSelector.bind(this, 9)}> Starship 9 </button>
+       </header>
+
     </div>
-  );
+    );
+  }
 }
+ 
 
 export default App;
